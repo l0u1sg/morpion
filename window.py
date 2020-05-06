e46c37fd300aa448e798568a09f8a07f8255513f
Louis GALLET <46707587+louisgallet95@users.noreply.github.com>
2020-05-06 12:05:21 +0200

    New file

--- window.py
+++ window.py
@@ -2,3 +2,12 @@ import pygame
 (width, height) = (600, 700)
 screen = pygame.display.set_mode((width, height))
 pygame.display.flip()
+pygame.display.set_caption('Morpion game')
+screen.fill((0, 0, 0))
+background = pygame.image.load('morpion.png')
+screen.blit(background, (0, 0))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
